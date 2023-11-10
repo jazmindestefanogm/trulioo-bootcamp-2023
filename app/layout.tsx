19217d0ecb750602c782ca98c597f0984c338568
PedroLTC <pedroltch@gmail.com>
2023-11-10 12:01:07 -0300

    feat: Create the Theme of colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import HomePage from "./page";
+import { Providers } from "./provider";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
-
-export default function Home() {
+import { Box, Button } from "@chakra-ui/react";
+import React from "react";
+const page = () => {
   return (
-    <main>
-      <h1>home</h1>
-    </main>
+    <Box w="500px" h="500px" bgColor={"brand.900"}>
+      <Button bg={"brand.700"}>Click me</Button>
+    </Box>
   );
-}
+};
+export default page;
