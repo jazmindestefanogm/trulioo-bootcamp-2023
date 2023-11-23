f7e37ead2f216e06e82df7c7e2b40a17b49f5673
LuisTorresGM2Dev <luis.torres@gm2dev.com>
2023-11-23 14:16:36 -0300

    fix: delete the H1 on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import { FormVerification } from "./components/FormVerification";
 export default function Home() {
   return (
     <main>
-      <h1>home</h1>
-      <FormVerification/>
+      <FormVerification />
     </main>
   );
 }
